e907bc0c3b02f7367e6459eb26e0d6ce05d121d5
lukesdm <2824165+lukesdm@users.noreply.github.com>
2022-01-31 07:10:54 +0100

    Suitability legend

--- weighted-overlay/map/js/lm-custom.js
+++ weighted-overlay/map/js/lm-custom.js
@@ -1,18 +1,25 @@
+function makeEntriesForRaster(rEntries) {
+    return rEntries.map(rEntry => ({ symbol: `<span style="color:${rEntry.color}">&nbsp;█</span>`, description: rEntry.description }));
+}
+
 function buildLegend(entries) {
     entriesMarkup = entries.reduce((acc, entry) => acc + `<tr><td>${entry.symbol}</td><td>${entry.description}</td></tr>`, "");
     return `<table>${entriesMarkup}</table>`
 }
 
 let suitabilityEntries = [
-    { symbol: "🐍", description: "snake" },
-    { symbol: "🐻", description: "bear" },
-    { symbol: "🐪", description: "camel" },
+    { color: "#FEDF9A", description: "1" },
+    { color: "#F59053", description: "2" },
+    { color: "#E9F025", description: "3" },
+    { color: "#00DBA4", description: "4" },
+    { color: "#00CC52", description: "4.5" },
+    { color: "#00FF00", description: "5" },
 ];
 
 function addLegend() {
     entryIndex = 1; // TODO: Parameterize
     const container =  document.querySelector(`div.leaflet-control-layers-overlays > label:nth-child(${entryIndex}) > div > span`);
-    container.innerHTML = container.innerHTML + buildLegend(suitabilityEntries);
+    container.innerHTML = container.innerHTML + buildLegend(makeEntriesForRaster(suitabilityEntries));
     
 }
 
